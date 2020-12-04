21763b03761d25f9b7ef0cfbf4de2ee19e3d562d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-12-05 00:27:57 +0900

    初期化

--- src/app/App.kt
+++ src/app/App.kt
@@ -2,24 +2,11 @@ package app
 
 import react.*
 import react.dom.*
-import logo.*
-import ticker.*
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        div("App-header") {
-            logo()
-            h2 {
-                +"Welcome to React with Kotlin"
-            }
-        }
-        p("App-intro") {
-            +"To get started, edit "
-            code { +"app/App.kt" }
-            +" and save to reload."
-        }
-        p("App-ticker") {
-            ticker()
+        h1{
+            +"Hello World"
         }
     }
 }
--- src/index/index.kt
+++ src/index/index.kt
@@ -3,7 +3,7 @@ package index
 import app.*
 import kotlinext.js.*
 import react.dom.*
-import kotlin.browser.*
+import kotlinx.browser.*
 
 fun main(args: Array<String>) {
     requireAll(require.context("src", true, js("/\\.css$/")))
