7d4fcf9b6a41346661a1f4805e1762a7c080d72c
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-12-05 00:50:22 +0900

    繰り返し処理について

--- src/app/App.kt
+++ src/app/App.kt
@@ -5,9 +5,15 @@ import react.dom.*
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        h1{
-            +"Hello World"
+        for(i in 0..5){
+            h1{
+                +"${square(i)}"
+            }
         }
+    }
+
+    fun square(num: Int): Int{
+        return num*num
     }
 }
 
